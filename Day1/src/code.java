ed1502f8d8f7490b0850cd60be1d55a2e1000de3
Stephanie Riggs <stephanieriggs256@gmail.com>
2022-12-03 23:47:35 +0000

    Rearranges files, adds basic structure to code

---
+++ Day1/src/code.java
@@ -0,0 +1,12 @@
+public class code {
+
+    public static void main(String[] args) {
+        code run = new code();
+        System.out.printf(String.valueOf(run.mostCaloriesNum()));
+    }
+
+    public int mostCaloriesNum() {
+        int totalCalories = 0;
+        return totalCalories;
+    }
+}
